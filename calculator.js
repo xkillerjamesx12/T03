cc0dfb9d3279947e24e75202dfb93bf1b56370e8
James <hoyinchui5@gmail.com>
2024-01-31 18:34:42 +0800

    unfinish

---
+++ calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function()
+    {
+    let displayValue = document.querySelector(".displayValue");
+    let operations = document.querySelectorAll(".operators");
+    let pad = document.querySelectorAll(".pad");
+    document.write(displayValue);
+    document.write(operations);
+    document.write(pad);
+    // we now have access to the DOM tree!
+    // set up your initial document event handlers here.
+    };
