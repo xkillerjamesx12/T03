15f78fea25ba497c9d99734f8d829c9d61264361
James <hoyinchui5@gmail.com>
2024-02-02 16:09:41 +0800

    god bless me

--- calculator.js
+++ calculator.js
@@ -8,11 +8,19 @@ let equal = document.getElementsByClassName('.operator equal');
 function numberOnClick(digitBtns) {
     console.log(digitBtns);
  } 
-
+function operationOnClick(digitBtns) {
+    console.log(digitBtns);
+ } 
+function clearOnClick(digitBtns) {
+    console.log(digitBtns);
+ } 
+function equalOnClick(digitBtns) {
+    console.log(digitBtns);
+ } 
 document.addEventListener('DOMContentLoaded', function()
     {
-        let digitBtns =number;
+        let digitBtns = number;
         for( let i=0; i<digitBtns.length; i++ ) {
-            digitBtns[i].addEventListener('click', numberOnClick(digitBtns[i]));
+            digitBtns[i].addEventListener('click', console.log(digitBtns[i]));
         };
     });
