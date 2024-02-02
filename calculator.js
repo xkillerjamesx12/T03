717d357df9f27dce19d6fe09742370cede35c94f
James <hoyinchui5@gmail.com>
2024-02-02 16:02:48 +0800

    god bless me

--- calculator.js
+++ calculator.js
@@ -5,13 +5,14 @@ let number = document.getElementsByClassName("number");
 let clear = document.getElementById('clear');
 let equal = document.getElementsByClassName('.operator equal');
 
-function numberOnClick() {
-    console.log("number");
+function numberOnClick(digitBtns) {
+    console.log(digitBtns);
  } 
+
 document.addEventListener('DOMContentLoaded', function()
     {
         let digitBtns =number;
         for( let i=0; i<digitBtns.length; i++ ) {
-            digitBtns[i].addEventListener('click', numberOnClick);
+            digitBtns[i].addEventListener('click', numberOnClick(digitBtns[i]));
         };
     });
